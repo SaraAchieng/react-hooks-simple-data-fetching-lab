f3f651d928afafa05f582c9608e3b01819eac9e9
Sara Achieng <achieng997@gmail.com>
2024-08-06 09:40:54 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+// const API_URL = "https://dog.ceo/api/breeds/image/random"
+
+
+function App() {
+    const [image, setImage] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <img src = {image} alt = "A Random Dog"/>
+    )
+    
+    
+}
+
+export default App;
